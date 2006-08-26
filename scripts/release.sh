9bb447d5e899097d5bd425e027d03d8843c69770
pjotr <pjotr@63c4e4d7-b515-0410-a698-96a6ba135bec>
2006-08-26 12:23:01 +0000

    Make release file (.tgz)
    git-svn-id: http://rtags.rubyforge.org/svn@10 63c4e4d7-b515-0410-a698-96a6ba135bec

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,3 +3,7 @@
 if [ ! -d test ]; then echo "Run from base directory!" ; exit ; fi
 
 ruby ./scripts/rtags.gemspec
+
+version=`cat ./bin/VERSION`
+tar cvzf rtags-$version.tgz --exclude *.svn* bin/* test/* doc/* install.sh README TODO LICENSE.txt  RELEASENOTES
+
