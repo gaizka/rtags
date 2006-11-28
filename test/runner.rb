8247f20425a141316b4dd173a30154d2a19d460f
pjotr <pjotr@63c4e4d7-b515-0410-a698-96a6ba135bec>
2006-11-28 08:34:24 +0000

    Added Class.new tags for initialize methods
    git-svn-id: http://rtags.rubyforge.org/svn@20 63c4e4d7-b515-0410-a698-96a6ba135bec

--- test/runner.rb
+++ test/runner.rb
@@ -3,6 +3,14 @@
 
 require 'test/unit'
 
+if ARGV.shift=='-i'
+  Dir.chdir('regression')
+  print `rm -v *.expect`
+  print `rename 's/tags$/tags.expect/g' *.tags`
+  print `rename 's/TAGS$/TAGS.expect/g' *.TAGS`
+  exit
+end
+
 # run regression tests
 
 def regression_test basedir,file
